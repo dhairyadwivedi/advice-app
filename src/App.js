e3018fc2023a7bc68a04d0c79b37c7d6072bdf1f
dhairyadwivedi <dhairiya96@gmail.com>
2020-03-29 23:18:49 +0530

    fetch & display data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import { FaTwitter } from "react-icons/fa";
+import axios from "axios";
 
 import "./App.css";
 
@@ -8,11 +9,50 @@ export default class App extends Component {
     advice: ""
   };
 
-  componentDidMount(){
-      console.log('component did mount')
+  componentDidMount() {
+    this.fetchData();
   }
 
+  fetchData = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then(response => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch(error => {
+        console.logg(error);
+      });
+  };
+
   render() {
-    return <div>APP</div>;
+    let shareTweet =
+      "https://twitter.com/intent/tweet?text=" + this.state.advice;
+    return (
+      <div className="app">
+        <h1 id="heading">FREE ADVICE</h1>
+        <h2 id="subheading">
+          This app gives free advice, just like your relatives but actually
+          useful and less condescending.
+        </h2>
+        <div className="card">
+          <h1 className="advice">{this.state.advice}</h1>
+          <div className="buttonContainer">
+            <button className="button">Get Advice</button>
+            <button id="tweet" className="ui inverted basic button">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                class="twitter-share-button"
+                href={shareTweet}
+              >
+                <FaTwitter color="white" className="share" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
